9347945ce6a643e693eb8e9b9f3e59683377c1a4
Yuki97877 <205409539+Yuki97877@users.noreply.github.com>
2025-07-31 10:48:24 +0900

    Update humanoid_run.py

--- pybulletgym/examples/humanoid_run.py
+++ pybulletgym/examples/humanoid_run.py
@@ -1,10 +1,14 @@
 import pybullet_envs
 import gym
 import time
+from stable_baselines3 import PPO
 
 # Humanoid環境の読み込み
 env = gym.make("HumanoidFlagrunHarderBulletEnv-v0")
 obs = env.reset()
+
+# 学習済みモデルの読み込み
+model = PPO.load("models/humanoid_model")
 
 # 10000ステップ分動かす（お好みで増減OK）
 for _ in range(10000):
