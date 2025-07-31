fa1e0f09ecf30253485e109b35f328d73940ce76
Yuki97877 <205409539+Yuki97877@users.noreply.github.com>
2025-07-31 10:01:54 +0900

    Create train_humanoid.py

---
+++ pybulletgym/examples/train_humanoid.py
@@ -0,0 +1,15 @@
+from stable_baselines3 import PPO
+import gym
+import pybullet_envs
+
+# 環境を作成
+env = gym.make("HumanoidFlagrunHarderBulletEnv-v0")
+
+# モデル作成（PPOを使用）
+model = PPO("MlpPolicy", env, verbose=1)
+
+# 学習
+model.learn(total_timesteps=100_000)
+
+# モデルを保存
+model.save("models/humanoid_model")
